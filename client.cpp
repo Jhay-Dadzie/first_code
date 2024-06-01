eab03ad14234aa3ce55fdde165b149ef5e47eeab
Jhay-Dadzie <jhaygraphix0510@gmail.com>
2024-06-01 01:13:07 -0700

    This adds the client side

---
+++ client.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string password;
+    cout << "Please enter your password to log in";
+    cin >> password;
+    cout << "Welcome back";
+    return 0;
+}
