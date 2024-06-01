36de88ffad92a5ba4efa6ac75b38efa9e2503cc8
Jhay-Dadzie <jhaygraphix0510@gmail.com>
2024-05-31 22:53:41 -0700

    This allows the user to enter their name and date of birth

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 int main() {
     cout << "Hello world";
+    int age;
+    string name;
+    cout << "What is your name";
+    cin >> name;
+    cout << "How old are you?";
+    cin >> age;
+
     return 0;
 }
